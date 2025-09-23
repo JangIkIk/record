bc5035e7693f0dbb1c0050da0b59f276cb25a6e7
junik <wkdwnsdlr37@gmail.com>
2025-09-23 14:52:36 +0900

    마크다운 관련 Package 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   base: "/record/",
-   resolve:{
-     alias: [
-       { find: "@", replacement: "/src"},
-    ]
-  }
-})
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true, // Buffer polyfill 적용
+        }),
+      ],
+    },
+  },
+});
